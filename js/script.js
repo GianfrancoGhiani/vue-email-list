7763aaa931b0d0edf6c855ee86866016e2b5c1b7
GianfrancoGhiani <gianfranco.ghiani.gg@gmail.com>
2022-11-08 15:08:45 +0100

    complete

--- js/script.js
+++ js/script.js
@@ -8,10 +8,29 @@ https://flynn.boolean.careers/exercises/api/random/mail
 const app = Vue.createApp({
     data() {
         return {
-            
+            mailList: [],
+            created: false,
         }
     },
+    computed:{
+
+    },
     methods: {
-        
+        listCreation(){
+            const creatingMailList = [];
+            for(let i=0; i<10 ; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((answ)=>{
+                    creatingMailList.push(answ.data.response);
+                    if(creatingMailList.length === 10){
+                        this.mailList = {...creatingMailList};
+                        this.created = true
+                    }
+                });
+            }    
+        }
+    },
+    mounted() {
+        this.listCreation()
     },
 })
+app.mount('#app');
