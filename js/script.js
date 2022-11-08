356727bff49f5403d1ecb8a264150a55df62913e
GianfrancoGhiani <gianfranco.ghiani.gg@gmail.com>
2022-11-08 14:42:28 +0100

    scaffolder

---
+++ js/script.js
@@ -0,0 +1,17 @@
+'use strict'
+
+/*
+random mail API
+https://flynn.boolean.careers/exercises/api/random/mail
+*/
+
+const app = Vue.createApp({
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        
+    },
+})
